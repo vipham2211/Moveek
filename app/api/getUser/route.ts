c390ddddb359a3d28dac0f683523b5460398eacb
Vi Pham <vipham2211@gmail.com>
2023-09-30 20:42:00 +0700

    fix err type

--- app/api/getUser/route.ts
+++ app/api/getUser/route.ts
@@ -1,11 +1,11 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(): Promise<any> {
 	const userCookie = cookies().get("REMEMBERME");
 	if (!userCookie) return 
 	const userLogin = JSON.parse(userCookie.value);
 	
-  return NextResponse.json({ userLogin:Promise<any> });
+  return NextResponse.json({ userLogin });
   
 }
