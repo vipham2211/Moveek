f1b779cd8d368385bff0a46cf4d01450cdf39fae
Vi Pham <vipham2211@gmail.com>
2023-09-30 20:01:05 +0700

    fix route getUser

--- app/api/getUser/route.ts
+++ app/api/getUser/route.ts
@@ -1,15 +1,10 @@
 import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server';
-
-
 
 export async function GET() {
-	
 	const userCookie = cookies().get("REMEMBERME");
-	
 	if (!userCookie) return 
 	const userLogin = JSON.parse(userCookie.value);
 	
-
-  return NextResponse.json({ userLogin });
+  return Response.json({ userLogin })
+  
 }
