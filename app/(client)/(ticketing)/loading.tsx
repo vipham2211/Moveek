2dafac41b6cfc908adbcfa46fcbc38b4cd5ab404
Vi Pham <vipham2211@gmail.com>
2023-10-01 10:03:24 +0700

    add loading ticketing

---
+++ app/(client)/(ticketing)/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingComponent from '@/components/shared/LoadingComponent'
+import React from 'react'
+
+const loading = () => {
+  return (
+	<LoadingComponent/>
+  )
+}
+
+export default loading
